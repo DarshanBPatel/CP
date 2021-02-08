40ce4cfe0e4d283d42f4f36c482fceac7e97af74
DarshanBPatel <35736874+DarshanBPatel@users.noreply.github.com>
2021-02-08 14:27:52 +0530

    Add files via upload
    This is my coding library .

---
+++ Bridges in Graph.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define endl "\n"
+#define fr first
+#define sc second
+#define f(i, L, R) for (ll i = L; i < R; i++)
+int n;					 // number of nodes
+vector<vector<int>> adj; // adjacency list of graph
+vector<pair<int, int>> Bridges;
+vector<bool> visited;
+vector<int> tin, low;
+int timer;
+
+void dfs(int v, int p = -1)
+{
+	visited[v] = true;
+	tin[v] = low[v] = timer++;
+	for (int to : adj[v])
+	{
+		if (to == p)
+			continue;
+		if (visited[to])
+		{
+			low[v] = min(low[v], tin[to]);
+		}
+		else
+		{
+			dfs(to, v);
+			low[v] = min(low[v], low[to]);
+			if (low[to] > tin[v])
+			{
+				Bridges.push_back({v, to});
+			}
+		}
+	}
+}
+
+void find_bridges()
+{
+	timer = 0;
+	visited.assign(n, false);
+	tin.assign(n, -1);
+	low.assign(n, -1);
+	for (int i = 0; i < n; ++i)
+	{
+		if (!visited[i])
+			dfs(i);
+	}
+}
